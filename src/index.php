d0b984c6444805da3e7bef7d8364cd49c02455de
Mohsen Ahmadi <mohsen@ahmadi.pm>
2020-05-08 22:01:21 +0430

    translate some pages

--- src/index.php
+++ src/index.php
@@ -14,13 +14,13 @@ foreach($templates as $index=>$template)
 }
 ?>
 <!doctype html>
-<html class="no-js" lang="en-EN">
+<html class="no-js" lang="fa-IR">
 <head>
   <meta charset="utf-8">
   <base href="/">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Online planning poker</title>
-  <meta name="description" content="Scrumpoker online is an open source web implementation of planning poker for scrum teams to determine the complexity of stories. It aims to integrate ticketing systems like JIRA, Github or Gitlab.">  
+  <title>اسکرام پوکر آنلاین</title>
+  <meta name="description" content="اسکرام پوکر آنلاین، ابزاری برای تیم‌های اسکرام است که در جلسات برنامه‌ریزی اسپرینت می‌توانند از آن به رایگان استفاده کنند.">  
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link rel="apple-touch-icon" href="apple-touch-icon.png">
@@ -53,13 +53,6 @@ foreach($templates as $index=>$template)
    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
 
-<!--Github Fork Badge -->
-<div class="github-fork-ribbon-wrapper hidden-xs">
-  <div class="github-fork-ribbon">
-    <a target="_blank" href="https://github.com/Toxantron/scrumonline">Fork me on GitHub</a>
-  </div>
-</div>
-
 <!-- Top navigation bar -->
 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container-fluid navigation">
@@ -70,12 +63,14 @@ foreach($templates as $index=>$template)
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-    	<a class="navbar-brand" href="/">Scrum Poker</a>
+    	<a class="navbar-brand" href="/">اسکرام پوکر</a>
     </div>
     <div id="navbar" class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
       <?php foreach($navItems as $navItem): ?>
         <li data-toggle="collapse" data-target=".navbar-collapse.in"><a href="<?php echo $navItem->link ?>"><?php echo $navItem->navigationTag ?></a></li>
+        <li data-toggle="collapse" data-target=".navbar-collapse.in"><a href="https://fa.ahmadi.pm">یادداشت‌های فارسی</a></li>
+        <li data-toggle="collapse" data-target=".navbar-collapse.in"><a href="https://fa.ahmadi.pm/reading-list">ریدینگ لیست</a></li>
       <?php endforeach; ?>
       </ul>
     </div> <!--/.nav-collapse -->
